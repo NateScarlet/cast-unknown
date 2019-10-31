3b41de7977945707652bfc2fd37a894e0dd32bb3
NateScarlet <NateScarlet@Gmail.com>
2019-10-31 23:06:34 +0800

    build: fix node build

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ module.exports = (env, argv) => {
   const nodeConfig = createConfig();
   nodeConfig.target('node');
   nodeConfig.output.filename('index.node.js');
+  nodeConfig.output.libraryTarget('commonjs');
 
   const webConfig = createConfig();
   webConfig.output.library('castUnknown').libraryTarget('umd');
